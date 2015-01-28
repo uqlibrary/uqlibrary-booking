5f4c23c9f77760ba833e385f23c6680e40ad6c49
jannakha <j_malikova@yahoo.com>
2015-01-28 14:04:21 +1000

    updated to 0.5.4, updated functional tests, wct configuration

--- wct.conf.js
+++ wct.conf.js
@@ -1,4 +1,33 @@
 module.exports = {
   verbose: true,
-  browsers: ["chrome", "firefox", "safari"]
+  plugins: {
+    //local: {
+    //  browsers: ["chrome", "firefox"]
+    //},
+    sauce: {
+      browsers: [
+        {
+          "browserName": "safari",
+          "platform": "OS X 10.9",
+          "version": "7"
+        }
+        ,
+        {
+          "browserName": "firefox",
+          "platform": "Windows 8.1",
+          "version": "34"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+        {
+          "browserName": "chrome",
+          "platform": "OS X 10.9",
+          "version": "39"
+        }
+        ]
+    }
+  }
 };
